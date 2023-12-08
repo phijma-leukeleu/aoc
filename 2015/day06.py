8df5424058d98543f4d4b6d031d3a98eb0a801a9
Peter Hijma <phijma@leukeleu.nl>
2023-12-08 14:24:35 +0100

    Less loops, but slower

--- 2015/day06.py
+++ 2015/day06.py
@@ -7,21 +7,16 @@ lights_on = set()
 
 for instruction in instructions:
     x1, y1, x2, y2 = map(int, re.findall(r"\d+", instruction))
-    if "on" in instruction:
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            if "on" in instruction:
                 lights_on.add((i, j))
-    elif "off" in instruction:
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
+            elif "off" in instruction:
                 lights_on.discard((i, j))
-    else:  # toggle
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if (i, j) in lights_on:
-                    lights_on.remove((i, j))
-                else:
-                    lights_on.add((i, j))
+            elif (i, j) in lights_on:
+                lights_on.remove((i, j))
+            else:
+                lights_on.add((i, j))
 
 print(len(lights_on))
 
@@ -31,17 +26,14 @@ lights = [[0 for _ in range(1000)] for _ in range(1000)]
 
 for instruction in instructions:
     x1, y1, x2, y2 = map(int, re.findall(r"\d+", instruction))
-    if "on" in instruction:
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
+
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            if "on" in instruction:
                 lights[i][j] += 1
-    elif "off" in instruction:
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
+            elif "off" in instruction:
                 lights[i][j] = max(0, lights[i][j] - 1)
-    else:  # toggle
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
+            else:  # toggle
                 lights[i][j] += 2
 
 print(sum([sum(row) for row in lights]))
