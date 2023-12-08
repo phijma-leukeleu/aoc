35c381df222f589ea72cee4369f983bb8973b561
Peter Hijma <phijma@leukeleu.nl>
2023-12-08 13:57:12 +0100

    Solution day 6 - 2015

---
+++ 2015/day06.py
@@ -0,0 +1,47 @@
+import re
+
+with open("day06_input.txt") as file:
+    instructions = file.read().splitlines()
+
+lights_on = set()
+
+for instruction in instructions:
+    x1, y1, x2, y2 = map(int, re.findall(r"\d+", instruction))
+    if "on" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                lights_on.add((i, j))
+    elif "off" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                lights_on.discard((i, j))
+    else:  # toggle
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                if (i, j) in lights_on:
+                    lights_on.remove((i, j))
+                else:
+                    lights_on.add((i, j))
+
+print(len(lights_on))
+
+
+# Part 2
+lights = [[0 for _ in range(1000)] for _ in range(1000)]
+
+for instruction in instructions:
+    x1, y1, x2, y2 = map(int, re.findall(r"\d+", instruction))
+    if "on" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                lights[i][j] += 1
+    elif "off" in instruction:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                lights[i][j] = max(0, lights[i][j] - 1)
+    else:  # toggle
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                lights[i][j] += 2
+
+print(sum([sum(row) for row in lights]))
